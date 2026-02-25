60ae28461f7470bdedd82dbbec359e1ae16202e5
ShradhaV08 <sv4119@srmist.edu.in>
2026-02-25 11:11:26 +0530

    PalindromeCheckerApp

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,11 @@
+class UC1_PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        System.out.println("--- UC1: Application Entry & Welcome Message ---");
+        System.out.println("==============================");
+        System.out.println("   Palindrome Checker App     ");
+        System.out.println("   Version: 1.0.0             ");
+        System.out.println("   System initialized successfully");
+        System.out.println("==============================");
+    }
+}
