be4897c6854c6383fe218955b725790c6c93c51f
ShradhaV08 <sv4119@srmist.edu.in>
2026-02-25 11:30:11 +0530

    UC5:Stack-Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,29 +1,30 @@
 import java.util.Scanner;
+import java.util.Stack;
 
-class UC4_PalindromeCheckerApp {
+class UC5_PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        System.out.println("--- UC4: Character Array Based Palindrome Check ---");
+        System.out.println("--- UC5: Stack-Based Palindrome Checker ---");
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input Text: ");
         String word = scanner.nextLine();
 
-        char[] chars = word.toCharArray();
-        int start = 0;
-        int end = chars.length - 1;
-        boolean isPalindrome = true;
+        Stack<Character> stack = new Stack<>();
 
-        while (start < end) {
-            if (chars[start] != chars[end]) {
-                isPalindrome = false;
-                break;
-            }
-            start++;
-            end--;
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
         }
 
+        String stackReversed = "";
+        while (!stack.isEmpty()) {
+            stackReversed = stackReversed + stack.pop();
+        }
+
+        boolean isPalindrome = word.equals(stackReversed);
+
         System.out.println("Input Text          : " + word);
+        System.out.println("Reversed Text       : " + stackReversed);
         System.out.println("Is it a Palindrome? : " + isPalindrome);
 
         scanner.close();
