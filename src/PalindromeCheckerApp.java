a2c55c4cd1d7967cddb0feb42021856d12d683c9
ShradhaV08 <sv4119@srmist.edu.in>
2026-02-25 11:28:51 +0530

    UC4:Character Array Based Palindrome Check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,23 +1,29 @@
 import java.util.Scanner;
 
-class UC3_PalindromeCheckerApp {
+class UC4_PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        System.out.println("--- UC3: Palindrome Check Using String Reverse ---");
+        System.out.println("--- UC4: Character Array Based Palindrome Check ---");
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input Text: ");
         String word = scanner.nextLine();
 
-        String reversed = "";
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
+        char[] chars = word.toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
 
-        boolean isPalindrome = word.equals(reversed);
-
         System.out.println("Input Text          : " + word);
-        System.out.println("Reversed Text       : " + reversed);
         System.out.println("Is it a Palindrome? : " + isPalindrome);
 
         scanner.close();
