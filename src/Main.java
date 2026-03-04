4ae8b9881f9b656a5a086a27663fadd47c197266
ShradhaV08 <sv4119@srmist.edu.in>
2026-03-04 23:07:34 +0530

    UC8 - Linked List Based Palindrome Checker

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,67 @@
 import java.util.Scanner;
-import java.util.Deque;
-import java.util.ArrayDeque;
 
-class UC7_PalindromeCheckerApp {
+class UC8_PalindromeCheckerApp {
+
+    static class Node {
+        char data;
+        Node next;
+
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
 
     public static void main(String[] args) {
-        System.out.println("--- UC7: Deque-Based Optimized Palindrome Checker ---");
+        System.out.println("--- UC8: Linked List Based Palindrome Checker ---");
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input Text: ");
         String word = scanner.nextLine();
 
-        Deque<Character> deque = new ArrayDeque<>();
+        Node head = null;
+        Node tail = null;
 
         for (int i = 0; i < word.length(); i++) {
-            deque.offerLast(word.charAt(i));
+            Node newNode = new Node(word.charAt(i));
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
         }
 
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node prev = null;
+        Node current = slow;
+
+        while (current != null) {
+            Node nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        Node left = head;
+        Node right = prev;
         boolean isPalindrome = true;
 
-        while (deque.size() > 1) {
-            if (deque.pollFirst() != deque.pollLast()) {
+        while (right != null) {
+            if (left.data != right.data) {
                 isPalindrome = false;
                 break;
             }
+            left = left.next;
+            right = right.next;
         }
 
         System.out.println("Input Text          : " + word);
